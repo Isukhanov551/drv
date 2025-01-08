ddbcafa6ab108ebd81bb73f1d595597b76f4de30
Isukhanov551 <suhanov055@gmail.com>
2025-01-08 09:46:31 +0200

    Fix compile errors

--- sample_blc_device/request.c
+++ sample_blc_device/request.c
@@ -3,14 +3,19 @@
 #include <linux/blk-mq.h>
 #include "request.h"
 
-static void request_data(void)
+
+char* blc_buffer = NULL;
+
+static void block_read(sector_t sector, char* buffer, unsigned long bytes)
 {
     printk("BLK DEV: request  func !!!!!!");
+
     return;
 }
 
-static void recieve_data(void){
+static void block_write(sector_t sector, char* buffer, unsigned long bytes){
     printk("BLC DEV: recieve func");
+    return;
 }
 
 blk_status_t request_handler(struct blk_mq_hw_ctx* hctx, struct blk_mq_queue_data* bd)
@@ -18,17 +23,22 @@ blk_status_t request_handler(struct blk_mq_hw_ctx* hctx, struct blk_mq_queue_dat
     printk("Request IO message !!!!!!!!");
 
     struct request* rq = bd->rq;
-    //struct my_block_dev* dev = q->queuedata;
-    request_data();
-    recieve_data();
+    //struct my_block_dev* dev = rq->queuedata;
+    //struct bio_vec bvec;
+    sector_t sc = blk_rq_pos(rq);
+    unsigned long nbts = blk_rq_cur_bytes(rq);
+
+    //block_read();
+    //block_write();
+    
     blk_mq_start_request(rq);
     if(rq_data_dir(rq) == READ)
     {
-        recieve_data();
+        block_read(sc, blc_buffer, nbts);
     }
     else if(rq_data_dir(rq) == WRITE)
     {
-        request_data();
+        block_write(sc, blc_buffer, nbts);
     }
     blk_mq_end_request(rq, BLK_STS_OK);
 
